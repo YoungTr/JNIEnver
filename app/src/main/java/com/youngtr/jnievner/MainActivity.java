e871228070208162a75c9d32fd4e743740619f5a
tangyan <tangyan@tuya.com>
2022-02-17 10:44:13 +0800

    first commit

---
+++ app/src/main/java/com/youngtr/jnievner/MainActivity.java
@@ -0,0 +1,36 @@
+package com.youngtr.jnievner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.youngtr.jnievner.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'jnievner' library on application startup.
+    static {
+        System.loadLibrary("jnievner");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'jnievner' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
